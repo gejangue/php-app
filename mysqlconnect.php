528da35f995cd722aed390517fe12f69db346b71
gejangue <64191861+gejangue@users.noreply.github.com>
2020-05-04 16:37:59 +0200

    add DB parameter

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,12 +1,27 @@
 <?php
 
-$dbuser = 'mysqlkurs';
-$dbpass = 'KC00iT8s0NOSpl5S';
-$dbname = 'mysql_kurs';
-// Hier käme der Link zum Host
-$dbhost = '...';
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("databaseuser");
+$dbpwd = getenv("databasepassword");
+$dbname = getenv("databasename");
 
-$pdo = new PDO("mysql:host=localhost;dbname=$dbname", $dbuser, $dbpass);
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+} else {
+    printf("Connected to the database");
+}
+$connection->close();
+
+//$dbuser = 'mysqlkurs';
+//$dbpass = 'KC00iT8s0NOSpl5S';
+//$dbname = 'mysql_kurs';
+// Hier käme der Link zum Host
+//$dbhost = '...';
+
+$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 //var_dump($pdo);
 
 $query = $pdo->prepare("SELECT * FROM buecher");
